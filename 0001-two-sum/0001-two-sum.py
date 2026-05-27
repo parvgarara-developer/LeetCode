cddeb22f0eb22adf5fdb084d39f1898ff4790300
Parv Garara <parvgararapremier123@gmail.com>
2026-05-27 04:41:00 -0600

    Time: 2 ms (56.29%), Space: 13.1 MB (67.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        n = len(nums)
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return[i,j]
-        return []
+        dictionary = dict()
+        pos = 0
+        while pos < len(nums):
+            if (target - nums[pos]) not in dictionary:
+                dictionary[nums[pos]] = pos
+                pos += 1
+            else:
+                return [dictionary[target - nums[pos]], pos]
