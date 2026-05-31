12ce4da28ff68fe9a55243a795355b3d7b668e99
Parv Garara <parvgararapremier123@gmail.com>
2026-05-31 14:53:49 +0530

    Time: 8 ms (78.82%), Space: 13 MB (12.18%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,25 +1,16 @@
-class Solution(object):
+class Solution:
     def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
-        m, n = len(s), len(p)
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
-
-        for j in range(1, n + 1):
-            if p[j - 1] == '*':
-                dp[0][j] = dp[0][j - 2]
-
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if p[j - 1] in {s[i - 1], '.'}:
-                    dp[i][j] = dp[i - 1][j - 1]
-                elif p[j - 1] == '*':
-                    dp[i][j] = dp[i][j - 2]
-                    if p[j - 2] in {s[i - 1], '.'}:
-                        dp[i][j] = dp[i][j] or dp[i - 1][j]
-
-        return dp[m][n]
+        memo = {}
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if j == len(p):
+                return i == len(s)
+            first_match = i < len(s) and (p[j] == s[i] or p[j] == '.')
+            if j + 1 < len(p) and p[j + 1] == '*':
+                ans = dp(i, j + 2) or (first_match and dp(i + 1, j))
+            else:
+                ans = first_match and dp(i + 1, j + 1)
+            memo[(i, j)] = ans
+            return ans
+        return dp(0, 0)
