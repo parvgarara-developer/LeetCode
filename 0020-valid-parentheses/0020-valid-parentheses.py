baa46737538f7a2c7a8985a543f085b9b4a98e6b
Parv Garara <parvgararapremier123@gmail.com>
2026-07-08 13:16:27 +0530

    Time: 0 ms (100%), Space: 12.5 MB (69.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        if len(s)%2 == 1:
+            return False
+        else:
+            stack = []
+            mapping = {")": "(", "}": "{", "]": "["}
+            for char in s:
+                if char in mapping:
+                    top_element = stack.pop() if stack else '#'
+                    if mapping[char] != top_element:
+                        return False
+                else:
+                    stack.append(char)            
+            return not stack
